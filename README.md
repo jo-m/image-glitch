d08713325dd4b6ce66eb4d900f1b1a2276946440
Jonathan Müller <j.mueller@apoveda.ch>
2015-09-16 13:29:27 +0200

    add code

---
+++ README.md
@@ -0,0 +1,6 @@
+# ImageGlitch
+Animate image glitches at webpages. A sample HTML file is included (`sample.html`).
+
+## Attribution
+Original code is from [here](http://codepen.io/lbebber/pen/EjVPao). I refactored
+and modified it to make it usable for use in a website as JS library.
---
+++ image-glitch.js
@@ -0,0 +1,69 @@
+/* Copyright (C) 2015 Jonathan Müller - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, visit: https://github.com/jo-m/image-glitch
+ *
+ * Code originally based on http://codepen.io/lbebber/pen/EjVPao
+ */
+
+ var ImageGlitch = function(img){
+  // probability for glitch
+  this.pGlitch = 0.2;
+  // glitch period
+  this.intervalMs = 1000;
+  // after a glitch, wait for this time until original image is shown again
+  this.restoreMs = 25;
+
+  // data segment at begin to spare from corruption
+  this.headerSize = 2200;
+  this.img = img;
+  this.data = null;
+}
+
+ImageGlitch.prototype.start = function() {
+  this.data = this.getBase64Image(this.img);
+  this.img.setAttribute('src', this.data)
+
+  setInterval(this.glichImage.bind(this), this.intervalMs);
+}
+
+ImageGlitch.prototype.getBase64Image = function(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  return canvas.toDataURL("image/jpeg");
+}
+
+ImageGlitch.prototype.flipBytes = function(data, index) {
+  return data.substr(0, index) +
+    data.charAt(index + 1) +
+    data.charAt(index) +
+    data.substr(index + 2);
+}
+
+ImageGlitch.prototype.corruptData = function(data) {
+  var corrupted = data;
+  var nErrors = Math.random() * 100;
+  for (var i = 0; i < nErrors; i++) {
+    var index = this.headerSize +
+      Math.round(Math.random() * (corrupted.length - this.headerSize - 1));
+    corrupted = this.flipBytes(corrupted, index);
+  }
+  return corrupted;
+}
+
+ImageGlitch.prototype.glichImage = function() {
+  if (Math.random() < this.pGlitch) {
+    this.img.setAttribute('src', this.corruptData(this.data))
+    setTimeout(this.restoreOriginalImage.bind(this), this.restoreMs)
+  }
+}
+
+ImageGlitch.prototype.restoreOriginalImage = function() {
+  this.img.setAttribute('src', this.data);
+}
